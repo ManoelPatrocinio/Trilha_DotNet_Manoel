625eebb397a10bc2a93caa4a103b382e5c5a7259
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-11-23 17:30:12 -0300

    add aula da semana 3, dia 23 novembro

---
+++ semana3/atvEmSala/aula23_11/Program.cs
@@ -0,0 +1,27 @@
+﻿#region Expressão lambda
+
+    string[] people = { "Manoel", "Marcela", "John"};
+
+    char letter = 'M';
+    Console.WriteLine($"People with name started with '{letter}': {string.Join(", ", people.Where(x => x.StartsWith(letter)))}");
+#endregion
+
+#region Linq
+
+List<int> list = new() {1,2,3,4,5,6,7};
+
+var squareList = list.Select(x => x * x);
+
+Console.WriteLine($"Original List: {string.Join(", ", list)}"); 
+Console.WriteLine($"Original List: {string.Join(", ", squareList)}"); 
+
+var summedList = list.Select((x,index) => x + squareList.ElementAt(index));
+Console.WriteLine($"Summed List: {string.Join(", ", summedList)}");
+
+var listMultipleOfTree = list.Where(x => x % 3 == 0).ToList();
+
+listMultipleOfTree.AddRange(squareList.Where(x => x % 3 == 0).ToList());
+listMultipleOfTree.AddRange(summedList.Where(x => x % 3 == 0).ToList());
+Console.WriteLine($"List Multiple of Three: {string.Join(", ",listMultipleOfTree)}");
+Console.WriteLine($"List Multiple of Three: {string.Join(", ",listMultipleOfTree.Order())}");
+#endregion
---
+++ semana3/atvEmSala/aula23_11_exec/Program.cs
@@ -0,0 +1,15 @@
+﻿// aula 5 - questão 5
+List <( string name, int height)> tupleList = new();
+
+tupleList.Add(("Manoel", 1000));
+tupleList.Add(("John", 1000));
+tupleList.Add(("Marcela", 1000));
+
+int soma = 0;
+// var media = tupleList.Select( args => soma += args.height ).ToList();
+
+tupleList.ForEach( args => soma += args.height );
+Console.WriteLine("soma " + soma);
+
+Func<int,int,int> media = (soma,qtd) => soma / qtd;
+Console.WriteLine($"media {media(soma,tupleList.Count)}");
