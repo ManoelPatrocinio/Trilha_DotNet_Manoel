1e53ebe5901a887354d8da603a1f3b1fa168da41
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-11-30 15:35:00 -0300

    commit 1

---
+++ av_individual/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Advogado
+{
+
+    public Advogado(string _nome, int _dataNasc, string _cor)
+    {
+         = _model;
+        Ano = _ano;
+        cor = _cor;
+        IdadeVeiculo = 
+    }
+
+    public string Nome { get; set; }
+    public Date DataNasc { get; set; }
+    public string CPF { get; set; }
+    public string CNA { get;set }
+
+}
+
+
+
+namespace Application
+{
+
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
