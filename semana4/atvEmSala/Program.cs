b3989b13cd964893ab41729ba40c6f491d6cb077
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2023-11-30 15:23:48 -0300

    atv em sala 30_11

---
+++ semana4/atvEmSala/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Veiculo
+{
+
+    public Veiculo(string _model, int _ano, string _cor)
+    {
+        Modelo = _model;
+        Ano = _ano;
+        cor = _cor;
+        IdadeVeiculo = 
+    }
+
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+    public string Cor { get; set; }
+    public string IdadeVeiculo { get; }
+
+}
+
+
+
+namespace Application
+{
+
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            Veiculo v1 = new Veiculo("exe","2022","cinza");
+            Console.WriteLine($"Modelo: {v1.Modelo}, Ano: {v1.Ano}, cor {v1.Cor}");
+        }
+    }
+}
+
